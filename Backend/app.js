2280a4740a37b17e1cd7bcfbbbc395b0141847a1
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2024-12-26 00:38:36 +0530

    db setup

--- Backend/app.js
+++ Backend/app.js
@@ -4,9 +4,10 @@ dotenv.config();
 const express = require("express");
 const cors = require('cors')
 const app = express();
-
+const connectToDB = require("./db/db")
 app.use(cors());
 
+connectToDB();
 app.get('/', (req, res)=>{
     res.send("Hello world")
 })
