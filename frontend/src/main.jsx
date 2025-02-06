1e26cde47be3f641e5357b9d6bd16e25f7bf46c0
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2025-02-06 20:56:58 +0530

    user context done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { UserContext } from "./context/UserContext.jsx";
+import { CaptainDataContext } from "./context/CapatainContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <UserContext>
+    <CaptainDataContext>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </CaptainDataContext>
+    </UserContext>
   </StrictMode>
 );
